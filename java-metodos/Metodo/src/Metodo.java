57226dee9c6f9f33fc9636cc3c3d880feabee252
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-02-22 19:25:27 -0300

    aula metodos

---
+++ java-metodos/Metodo/src/Metodo.java
@@ -0,0 +1,23 @@
+public class Metodo {
+    public static void main(String[] args) {
+        
+        /*
+         * Criterios para criação de Metodos
+         * 
+         * Deve ser nomeado como verbo Ex: calcular, somar, andar.
+         * Seguir o padrão camelCase iniciar com minuscula e a
+         * inicial da segunda palavra maiuscula
+         * 
+         * Ex:
+         * somar(int a, int b){}
+         * abrirConexao(){}
+         * concluirProcessamento(){}
+         * 
+         ! findById(ind id){} -> 
+         ! calcularImprimir(){} -> nao criar metodos com mais de uma função ou responsabilidade
+         * Caso o metodo não retorne nenhum valor, será retornado a palavra chave void
+         * 
+         */
+
+    }
+}

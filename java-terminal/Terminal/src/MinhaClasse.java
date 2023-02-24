fc2ed82c4d4ee3c0bc65fb116e07e717aebfb385
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-02-24 10:56:30 -0300

    java Terminal

---
+++ java-terminal/Terminal/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Executei no terminal!!!");
+    /*
+     * 
+     * Acessa o diretorio no prompt de comand e acessa a pasta onde se encontra o arquivo class
+     * mais não digita a extensão
+     * executa digitando java Nome_Da_Classe
+     */
+    
+    }
+}

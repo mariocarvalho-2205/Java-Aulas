bc42614ac5d1316e8fb4978d9101cc66a8c179b8
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-03-02 20:40:27 -0300

    Exercicio nome e idade

---
+++ java-loops-e-arrays/src/loops/exercicio/Ex1_NomeEIdade.java
@@ -0,0 +1,27 @@
+package loops.exercicio;
+
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Ex1_NomeEIdade {
+        public static void main(String[] args) {
+            //System.out.println("teste");
+            Scanner scan = new Scanner(System.in);
+
+            String nome;
+            int idade;
+
+            while (true) {
+                System.out.println("Nome: ");
+                nome = scan.next();
+                if (nome.equals("0")) break;
+
+                System.out.println("Idade: ");
+                idade = scan.nextInt();
+            }
+
+            System.out.println("Continuando...");
+        }
+}

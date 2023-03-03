3961aea189299ad44138e161c0a986e609267b3c
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-03-02 21:02:48 -0300

    Exercicio Nota finalizado

---
+++ java-loops-e-arrays/src/loops/exercicio/Ex2_Nota.java
@@ -0,0 +1,23 @@
+package loops.exercicio;
+
+import java.util.Scanner;
+
+public class Ex2_Nota {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+            
+            int nota;
+
+            System.out.println("Nota: ");
+            nota = scan.nextInt();
+
+            while (nota < 0 || nota > 10) {
+                System.out.println("Nota invalida!!! Digite Novamente: ");
+                nota = scan.nextInt();
+            }
+            
+
+            System.out.print("Sua nota foi ->  " + nota);
+    }    
+}
